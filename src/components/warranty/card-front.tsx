dfdf07198fe5272c4e3933240b8c2b13f80d4722
Abhishek Maurya <abhishekmaurya676@gmail.com>
2026-05-30 14:25:19 +0530

    gix

--- src/components/warranty/card-front.tsx
+++ src/components/warranty/card-front.tsx
@@ -191,17 +191,17 @@ export default function CardFront({
               />
             </div> */}
             {/* FDA Seal */}
-            {/* <div className="lex items-center justify-center" title="FDA Approved">
+            <div className="lex items-center justify-center" title="FDA Approved">
               <img
-                src="/fda-approved.jpg"
+                src="/ivoclar.png"
                 alt="FDA Approved"
-                className="w-8 h-8 object-contain rounded-[50%]"
+                className="w-9 h-9 object-contain rounded-[50%]"
                 style={{
                   filter:
                     "invert(1) brightness(1.6) drop-shadow(rgba(255, 255, 255, 0.35) 0px 0px 2px)",
                 }}
               />
-            </div> */}
+            </div>
 
             {/* ISO Seal */}
             <div className="flex items-center justify-center" title="ISO 9001:2015 Certified">
